b20d897f9786129d0f7c1882f7d409a3845288bf
Shessana Buissreth <shessanab15@gamail.comt>
2022-04-08 12:09:58 -0400

    added files

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+const tags = document.querySelector('.data-tab-content');
+
+var aboutContent = document.getElementById('about-content');
+var aboutTab = document.getElementById('about-tab');
+
+aboutTab.addEventListener('click', function() {
+    toggleVisibility(aboutContent);
+})
+
+var aboutContent = document.getElementById('reservations-content');
+var aboutTab = document.getElementById('reservations-tab');
+
+aboutTab.addEventListener('click', function() {
+    toggleVisibility(aboutContent);
+})
+
+reservationsTab.addEventListener('click', function() {
+    toggleVisibility(reservationsContent);
+})
+
+contactTab.addEventListener('click', function() {
+    toggleVisibility(contactContent);
+})
+
+function toggleVisibility(element) {
+    if (element.className === 'hidden') {
+        element.className = 'visible';
+    } else {
+        element.className = 'hidden';
+    }
+}
