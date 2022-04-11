457ab7b3b3d831aec1edd822164d88b93c7616d0
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-04-10 21:19:24 -0400

    removed unused js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,53 @@
-var aboutContent = document.getElementById('about-content');
-var aboutTab = document.getElementById('about-tab');
+// var question = document.querySelector('.faqQuestion');
+// console.log(question);
+// var answer = document.querySelector('.faqAnswer');
 
-aboutTab.addEventListener('click', function() {
-    toggleVisibility(aboutContent);
-})
+// function toggleVisibility(answer) {
+//     if (answer.className === 'faqAnswer hidden') {
+//         answer.className = 'faqAnswer visible';
+//     } else {
+//         answer.className = 'faqAnswer hidden';
+//     }
+// }
 
-var aboutContent = document.getElementById('reservations-content');
-var aboutTab = document.getElementById('reservations-tab');
+// question.addEventListener('click', function() {
+//     toggleVisibility(answer);
+// })
 
-aboutTab.addEventListener('click', function() {
-    toggleVisibility(aboutContent);
-})
 
-reservationsTab.addEventListener('click', function() {
-    toggleVisibility(reservationsContent);
-})
 
-contactTab.addEventListener('click', function() {
-    toggleVisibility(contactContent);
-})
 
-function toggleVisibility(element) {
-    if (element.className === 'hidden') {
-        element.className = 'visible';
-    } else {
-        element.className = 'hidden';
+// var displayHandler = function () {
+
+//     function toggleDisplayProperty(clickTargetEL, displayEl) {
+//         clickTargetEL.addEventListener('click', function() {
+//             toggleVisibility(displayEl);
+//         })
+//     }
+// }
+
+// var hiddenEl = answer[i].classList.replace("hidden", "visible");
+// question[i].outerHTML = hiddenEl;
+
+
+displayToggleHandler = function () {
+    var questions = document.querySelectorAll('.faqQuestion');
+    var answers = document.querySelectorAll('.faqAnswer');
+    console.log(questions);
+    console.log(answers);
+
+    for (var i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', function() {
+            console.log(answers[i]);
+            if (answers[i].classList.contains('hidden')) {
+                answers[i].className.replace('hidden', 'visible');
+                console.log('switched to visible')
+            } else {
+                answers[i].className.replace('visible', 'hidden');
+                console.log('switched to hidden')
+            }
+        })
     }
 }
+
+displayToggleHandler();
