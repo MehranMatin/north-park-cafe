20d9cd064ddf32d24bdff241ac27f6f9fbf47a06
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-04-10 22:52:55 -0400

    inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,54 @@
+
+/* Starter Modal Code */
+///////////////////////////
+// var modal = document.getElementsByClassName("faqAnswer");
+// var modalTrigger = document.getElementsByClassName("faqQuestion");
+// var closeModal = document.getElementsByClassName('closeModal')
+
+// modalTrigger.onclick = function() {
+//     modal.style.display = 'block';
+// }
+
+// closeModal.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//       modal.style.display = "none";
+//     }
+// }
+
+// modalTrigger
+
+
+/* TEST CODE TO HIDE/SHOW */
+///////////////////////////
+// displayToggleHandler = function () {
+//     var questions = document.querySelectorAll('.faqQuestion');
+//     var answers = document.querySelectorAll('.faqAnswer');
+//     console.log(questions);
+//     console.log(answers);
+
+//     for (var i = 0; i < questions.length; i++) {
+//         questions[i].addEventListener('click', function() {
+//             console.log(answers[i]);
+//             if (answers[i].classList.contains('hidden')) {
+//                 answers[i].className.replace('hidden', 'visible');
+//                 console.log('switched to visible')
+//             } else {
+//                 answers[i].className.replace('visible', 'hidden');
+//                 console.log('switched to hidden')
+//             }
+//         })
+//     }
+// }
+
+// displayToggleHandler();
+
+
+/* TEST CODE TO HIDE/SHOW */
+///////////////////////////
 // var question = document.querySelector('.faqQuestion');
 // console.log(question);
 // var answer = document.querySelector('.faqAnswer');
@@ -28,26 +79,3 @@
 
 // var hiddenEl = answer[i].classList.replace("hidden", "visible");
 // question[i].outerHTML = hiddenEl;
-
-
-displayToggleHandler = function () {
-    var questions = document.querySelectorAll('.faqQuestion');
-    var answers = document.querySelectorAll('.faqAnswer');
-    console.log(questions);
-    console.log(answers);
-
-    for (var i = 0; i < questions.length; i++) {
-        questions[i].addEventListener('click', function() {
-            console.log(answers[i]);
-            if (answers[i].classList.contains('hidden')) {
-                answers[i].className.replace('hidden', 'visible');
-                console.log('switched to visible')
-            } else {
-                answers[i].className.replace('visible', 'hidden');
-                console.log('switched to hidden')
-            }
-        })
-    }
-}
-
-displayToggleHandler();
